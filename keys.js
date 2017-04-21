640399c449d81050c4ee942d91d757c91b145d26
David L <lassiterda@gmail.com>
2017-04-21 11:44:37 -0400

    initial Commit of files, only code is switch case skeleton in liri.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: "OMI1JT2UlB8ja1ExmSa7k37n6",
+  consumer_secret: "PkJ6eR7wtkbGRpIsGFIZPNdJxlUgEBURIn67xNoqNJrNI8LnQu",
+  access_token_key: "855436309028122625-yYU3TJR2Aw0AvC1Bu1t0paSz6UWBBAX",
+  access_token_secret: "whF8SpbNhtAWPMUv5jZYImZbHjmRilUrVv4zcyDgCt4ce",
+}
---
+++ liri.js
@@ -0,0 +1,22 @@
+
+//require for keys
+const keys = require('./keys');
+
+
+
+switch (process.argv[2]) {
+  case "my-tweets":
+
+    break;
+  case "spotify-this-song":
+
+    break;
+  case "movie-this":
+
+    break;
+  case "do-what-it-says":
+
+    break;
+  default:
+    console.log("I'm sorry, I'm afraid I can't to that.")
+}
