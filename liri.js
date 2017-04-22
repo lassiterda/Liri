351415ff138de1cf5114cc5d927b51d01afe133a
David L <lassiterda@gmail.com>
2017-04-21 23:20:44 -0400

    completed movie-this function

--- liri.js
+++ liri.js
@@ -1,7 +1,37 @@
 
+//require for request
+const request = require('request');
 //require for keys
 const keys = require('./keys');
+//base64 encoded twitter auth str;
+const strTwitterAuth = Buffer.from(keys.twitterKeys.consumer_key + ":" + keys.twitterKeys.consumer_secret).toString('base64')
 
+var  movieThis = function(str) {
+
+  request("http://www.omdbapi.com/?t=" + str + "&plot=short&tomatoes=true", function(e, r, body) {
+    
+    let objBody = JSON.parse(body);
+    
+    if(e) {
+      console.log(e);
+    }
+    else if (objBody.Response === "False") {
+      movieThis("Mr. Nobody");
+    }
+    else { 
+      console.log("Title: " +  objBody.Title)
+      console.log("year: " + objBody.Year)
+      console.log("Rating (IMDB): " + objBody.imdbRating)
+      console.log("Country: " + objBody.Country)
+      console.log("Laguage: " + objBody.Language)
+      console.log("Plot: " + objBody.Plot)
+      console.log("Actors: " + objBody.Actors)
+      console.log("Rotten Tomatoes Rating: " + objBody.tomatoRating)
+      console.log("Rotten Tomatoes URL: " + objBody.tomatoURL)
+    }
+  });
+  
+}
 
 
 switch (process.argv[2]) {
@@ -12,7 +42,7 @@ switch (process.argv[2]) {
 
     break;
   case "movie-this":
-
+    movieThis(process.argv[3])
     break;
   case "do-what-it-says":
 
